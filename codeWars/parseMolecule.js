255abb86c6f89b4ce5c4cfc0d7c4ca582b474c8a
Ryan <ryanjlee12@gmail.com>
2015-08-26 13:24:12 -0700

    Refactor solution

--- codeWars/parseMolecule.js
+++ codeWars/parseMolecule.js
@@ -1,25 +1,36 @@
-
-
 function parseMolecule(formula) {
   var elements = formula.match(/[A-Z][a-z]?(?=\d?)|\d+|[()[\]{}]/g);
-  var result = {};
+  var atoms = {};
+  var stack = [];
   var brackets = {'(': ')', '[': ']', '{': '}'};
   var multiple = 1;
 
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].charCodeAt(0) >= 65 && elements[i].charCodeAt(0) <= 90) {
-      if (!result[elements[i]]) result[elements[i]] = 0;
-      if (parseInt(elements[i + 1]) {
-        result[elements[i]] += (parseInt(elements[i + 1]) * multiple);
+  function isChar(el) {
+    return el.charCodeAt(0) >= 65 && el.charCodeAt(0) <= 90;
+  }
+
+  function addAtom(el, num) {
+    if (!atoms[el]) atoms[el] = 0;
+    atoms[el] += parseInt(num) * multiple;
+  }
+
+  for (var i = elements.length - 1; i >= 0; i--) {
+    if (parseInt(elements[i])) {
+      if (isChar(elements[i - 1])) {
+        addAtom(elements[i -1], elements[i]);
       } else {
-        result[elements[i]] += 1;
-        i++;
+        stack.push(elements[i]);
+        multiple *= elements[i];
       }
+      i--;
+    } else if (isChar(elements[i])) {
+      addAtom(elements[i], 1);
     } else if (brackets[elements[i]]){
-      var endBracket = elements.lastIndexOf(brackets[elements[i]]);
-      if (parseInt(elements[endBracket + 1])) {
-        multiple = elements[endBracket + 1];
-      }
+      multiple /= stack.pop(elements[i])
+    } else {
+      stack.push(1);
     }
   }
+
+  return atoms;
 }
